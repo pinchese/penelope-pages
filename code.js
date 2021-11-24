42ddfe94306532adfb2f71db480ee39b16d5be75
pinchese <pinchese@protonmail.com>
2021-11-24 01:34:04 -0600

    merge dev code to production

---
+++ code.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    changeText();
+});
+
+function changeText() {
+    document.getElementById('textInput').value = 'this website is under construction...';
+}
